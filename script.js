b72c0d7300007044a76a43e3e4101f2666c61918
Apoh Eldrige <peldrige8@gmail.com>
2020-10-05 23:30:28 +0100

    added new service worker

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-console.log(arguments);
+// service worker registration
+
+if ("serviceWorker" in navigator) {
+  // console.log('Supported service worker');
+  window.addEventListener("load", () => {
+    navigator.serviceWorker
+      .register("./sw.js")
+      .then((reg) => {
+        console.log("Service worker: registered");
+      })
+      .catch((err) => console.error(err));
+  });
+}
